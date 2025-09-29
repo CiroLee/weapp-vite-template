8b177149ff24b3b2d19e32fadb11f4f68124d5cd
CiroLee <ciromail@126.com>
2025-09-29 23:14:58 +0800

    perf: 优化wxml文件格式化方式

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,6 @@
-export function hello() {
-  return 'world';
+export function getNavBarHeight() {
+  const windowInfo = wx.getWindowInfo();
+  const menuButtonInfo = wx.getMenuButtonBoundingClientRect();
+  // 导航栏高度 = 状态栏到胶囊的间距（胶囊距上未知-状态栏高度）* 2 + 胶囊高度 + 状态栏高度
+  return (menuButtonInfo.top - (windowInfo.statusBarHeight || 0)) * 2 + menuButtonInfo.height;
 }
